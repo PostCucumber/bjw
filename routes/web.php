f758a17bc1ffba4af76f6c267acb898fd888cce2
Dylan <johnston94@gmail.com>
2018-04-09 18:48:18 -0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ Route::get('/', function () {
     return view('front-page');
 });
 
+Route::get('/kiosk', function () {
+    return view('kiosk');
+});
+
 Route::get('/the-walk', 'TheWalkController@index')->name('blog');
-    
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
