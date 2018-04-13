d8a93bbcbe81ca094b30a5a6642c6a293e648c91
Dylan <johnston94@gmail.com>
2018-04-13 16:10:48 -0500

    wip

--- resources/views/pages/front-page.blade.php
+++ resources/views/pages/front-page.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="front-page-cover-image flex h-screen bg-cover bg-top">
+<div class="front-page-cover-image mt-12 flex h-screen bg-cover bg-top">
     <div class="absolute mt-24 ml-16 pin-t pin-l">
         <img class="w-48" src="https://bigjimswalk.com/wp-content/themes/vega/assets/img/BJW_logo-trans.png">
     </div>
--- resources/views/partials/foot.blade.php
+++ resources/views/partials/foot.blade.php
@@ -1,6 +1,6 @@
-<div class="bg-brand h-auto py-4">
-    <a href="" class="no-underline font-normal text-white hover:text-brand pr-4">The Walk</a>
-    <a href="" class="no-underline font-normal text-white hover:text-brand pr-4">About</a>
-    <a href="" class="no-underline font-normal text-white hover:text-brand pr-4">Testimonies</a>
-    <a href="" class="no-underline font-normal text-white hover:text-brand pr-4">Contact</a>
+<div class="bg-brand h-auto pl-8 py-4">
+    <a href="/the-walk" class="no-underline font-normal text-white hover:text-brand pr-4">The Walk</a>
+    <a href="/about" class="no-underline font-normal text-white hover:text-brand pr-4">About</a>
+    <a href="/testimonies" class="no-underline font-normal text-white hover:text-brand pr-4">Testimonies</a>
+    <a href="/contact" class="no-underline font-normal text-white hover:text-brand pr-4">Contact</a>
 </div>
